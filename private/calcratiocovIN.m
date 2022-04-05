332169b658176e92048bf660acf91201aff99cac
Ren M <marquez@caltech.edu>
2022-04-05 14:22:58 -0700

    Updates on the documentation of shake.m; Added first working version of calcratiocovIN.m - to be used for calculatiing the covariance matrix of the ratios when there is propagated uncertainty from the internal normalization.

---
+++ private/calcratiocovIN.m
@@ -0,0 +1,54 @@
+function V=calcratiocovIN(element,composition,errormodel,INisos)
+% Calculate the covariance matrix of the ratios with internal normalization
+% errormodel is the sub-structure for the element of interest that has 
+% INisos are the isotopes to use in the normalisation [n d]
+
+global ISODATA
+
+% Get rho values for the element of interest
+[a,ix]=ismember(INisos,ISODATA.(element).isonum);
+
+if sum(a) == 2
+    INisos = ix; % Convert input of normalization isotopes to indeces
+end
+
+m_i=ISODATA.(element).mass;
+m_j=m_i(INisos(1));
+m_k=m_i(INisos(2));
+
+rho_i=log(m_i./m_k)./log(m_j/m_k);
+
+% first normalise composition so it is really a composition (unit sum)
+composition=composition./sum(composition);
+
+meanbeams=composition.*errormodel.intensity;
+
+%if isequal(errormodel.type, 'fixed-sample')
+%meanbeams = meanbeams/(1.0 - prop);
+%end
+
+covbeams=calcbeamcov(meanbeams,errormodel);
+V=covbeamtoratioIN(meanbeams,covbeams,INisos,rho_i);
+
+function beamcov=calcbeamcov(meanbeams,errormodel)
+% the beam covariance matrix
+beamvar=errormodel.a + meanbeams.*errormodel.b + (meanbeams.^2).*errormodel.c;
+beamcov=diag(beamvar);
+
+function V=covbeamtoratioIN(meanbeams,covbeams,INisos,rho)
+% converts a covariance matrix for beams to one for ratios
+% INisos
+% assumes last row and column of M correspond to denominator
+di=INisos(2);
+isonums=1:length(meanbeams);
+ni=isonums(isonums~=di);
+
+n=meanbeams(ni);
+d=meanbeams(di);
+r=rho(ni);
+
+M=covbeams([ni di],[ni di]);  % move denominator to end
+%A=[diag(repmat(1/d,1,length(n))) -n'./(d^2)];
+%A=[(1/d).*eye(length(n)) -n'./(d^2)];
+A=[diag((1/d).*ones(1,length(n))) -n'./(d^2).*(1-r)']
+V=(A*M)*(A');
--- shake.m
+++ shake.m
@@ -3,23 +3,28 @@ function shake(element,sample,eff,cycles,R,R_reference,T,deltat)
 % SHAKE   Sets up the parameters necessary for performing calculations
 % under the COSMO and ERRORCURVESPLIT functions
 %
-%  SHAKE(element,sample,eff,cycles,R)
+%  SHAKE(element,sample,eff,cycles,R,R_reference,T,deltaT)
 %             element -- the element of interest. 
-%             sample -- the amount of sample to be analyzed - in
+%                sample -- the amount of sample to be analyzed - in
 %             nanograms(ng)
 %             eff -- 2 x 1 array that contains the ionization efficiency of
-%             the two instruments used for the IC and ID measurements,
-%             respectively. Range from 0 to 1 
+%                the two instruments used for the IC and ID measurements,
+%                respectively. Range from 0 to 1 
 %                e.g. [0.01 0.005]
 %             cycles -- 2 x 1 array that contains the no. of cycles
-%             for the IC and ID measurements, respectively
+%                for the IC and ID measurements, respectively
 %                e.g. [0.01 0.005]
 %             R -- 2 x nisos array that contains the resistance values for
-%             the IC (1st row) and ID (2nd row) measurements, respectively.
-%             By default, all resistors are 10^11 ohms. 
+%                the IC (1st row) and ID (2nd row) measurements, respectively.
+%                By default, all resistors are 10^11 ohms. 
 %             R_reference -- reference resistance used for describing beam intensity.
 %                The total beam current is intensity/R_reference
 %                e.g. the 10 V default beam corresponds to 100 pA with R_reference=1e11 Ohms.
+%             T -- absolute temperature of the resistor. Defualt values is
+%                300K.  
+%             deltat -- integration time for each measuremebnt cycle. In
+%                seconds (s)
+%      
 %                
 % All of these parameters (or resulting derived values are stored in the
 % global ISODATA variable. 
@@ -76,7 +81,7 @@ errormodel.sample = sample;
 % NOTE: The 'intensity' parameter in the 'measured' and 'standard'
 % substructures are not yet set here, as it will be determined by the
 % splitting of the sample - which is a variable set during the calculation
-% of uncertainties by ERROCURVESPLIT.m
+% of uncertainties by ERRORESTIMATESPLIT.m
 
 % Set all error models to be of type 'fixed-total' 
 % and add 'cycles','effIC', and 'effID' parameter
@@ -90,11 +95,11 @@ errormodel.measured.eff  = eff(2); % efficiency of the ID measurement
 errormodel.standard.cycles  = cycles(1); % no. of cycles of the IC measurement
 errormodel.measured.cycles  = cycles(2); % no. of cycles of the ID measurement
 
-aIC = 4*k*T*R_reference^2./(deltat*R(1,:));                 % For IC Measurement
-aID = 4*k*T*R_reference^2./(deltat*R(2,:));                 % For ID measurement
+aIC = 4*k*T*R_reference^2./(deltat*R(1,:)); % Johnson-Noise for IC Measurement
+aID = 4*k*T*R_reference^2./(deltat*R(2,:)); % Johnson-Noise for ID Measurement
 
-b = elementarycharge*R_reference/(deltat).*ones(1,nisos); % Counting statistics
-c = zeros(1,nisos);
+b   = elementarycharge*R_reference/(deltat).*ones(1,nisos); % Counting statistics
+c   = zeros(1,nisos);
 
 errormodel.standard.a=aIC;
 errormodel.standard.b=b;
